ac91d552bac9761f92f358270b1e35b840659696
Swayam Pandya <swayampandya1236@gmail.com>
2023-05-04 12:54:49 +0530

    user driver added

--- index.js
+++ index.js
@@ -2,22 +2,11 @@ if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition( async (position) => {
         const { latitude, longitude } = position.coords;
         console.log({longitude, latitude}, "$$$$$$");
-        document.getElementById('lon').innerHTML = latitude + ", " + longitude
         const response = await fetch(`https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode?location=${longitude},${latitude}&f=json&token=AAPK07c0979f73a64b5fb7d24d08aa3734fcH1aA3ZMjZT2sejVTWX-lgQmY6jE1nxGOeB7jZUUKSvvzZr7ZYAxRn0JU70UzGm6Y`)
         const data = await response.json();
 
         document.getElementById("loc").innerHTML =  data.address.City + ", " +data.address.Subregion + ", " + data.address.Region + ", " + data.address.Postal;
     })
-}
-
-const geolocation = async ()=>{
-    const response = await fetch(`https://ipapi.co/json/`)
-    data = await response.json()
-    console.log(data);
-    document.getElementById("iplan").innerHTML = data.longitude + ", " + data.latitude;
-    const response2 = await fetch(`https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode?location=${data.longitude},${data.latitude}&f=json&token=AAPK07c0979f73a64b5fb7d24d08aa3734fcH1aA3ZMjZT2sejVTWX-lgQmY6jE1nxGOeB7jZUUKSvvzZr7ZYAxRn0JU70UzGm6Y`)
-        const data2 = await response2.json();
-        document.getElementById("ip").innerHTML = data2.address.City + ", " +data2.address.Subregion + ", " + data2.address.Region+ ", " + data.address.Postal;
 }
 const handleEvent = (e) => {
     var inputValue = e.inputbox.value;
@@ -26,14 +15,13 @@ const handleEvent = (e) => {
         return false;
     }
     if (inputValue == "user") {
-        window.location.href = "http://127.0.0.1:5500/user.html"
+        window.location.href = "https://grand-wisp-e355d3.netlify.app/user.html"
     }
     if (inputValue == "driver") {
-        window.location.href = "http://127.0.0.1:5500/driver.html"
+        window.location.href = "https://grand-wisp-e355d3.netlify.app/driver.html"
     }
-    else {
-        window.location.reload();
-    }
+    // else {
+    //     window.location.reload();
+    // }
 
 }
-geolocation();
